ae4c4786974b5d1eef4689d45742daacd5a60378
anishpandit <0anishpandit@gmail.com>
2023-09-10 21:56:52 +0530

    Odd or Even Checker using python

---
+++ ODD_OR_EVEN_CHECK_USING_PYTHON.py
@@ -0,0 +1,7 @@
+print("Welcome to Odd & Even Number Check Machine.")
+number = int(input("Enter the Number:- "))
+
+if number % 2 == 0:
+    print("The Input Number is Even.")
+else:
+    print("The Input Number is Odd.")
